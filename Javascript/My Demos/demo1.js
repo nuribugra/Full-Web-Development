c7b78ffaa483aeb9c4c91f0ba35cf0a779aee317
Nuri Buğra Demir <bugranurid@gmail.com>
2022-10-23 16:22:55 +0300

    Bootstrap bölüm 1 bitti

---
+++ Javascript/My Demos/demo1.js
@@ -0,0 +1,13 @@
+var option1 = "Kahve";
+var option1Section1 = "Türk kahvesi";
+var option1Section2 = "Filtre kahve";
+var option1Section3 = "Cappuccino";
+var option1Section4 = "Espresso";
+var option1Section4 = "Americano";
+var option1Section4 = "Latte";
+var option1Section4 = "Mocha";
+
+var option2 = "Çay";
+var option2Section1 = "Yeşil çay";
+var option2Section2 = "Melisa çayı";
+var option2Section2 = "Bubble tea";
